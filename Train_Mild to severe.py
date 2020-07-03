c28acdea3d74b7d9d319c46a84ad3c959ee3196b
Ning-310 <64373769+Ning-310@users.noreply.github.com>
2020-07-03 13:59:40 +0800

    Update Train_Mild to severe.py

--- Train_Mild to severe.py
+++ Train_Mild to severe.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import roc_curve
 
-df = pd.read_excel('Data/OBCs.xlsx','Mild to severe')
+df = pd.read_excel('Data/Final biomarker combinations.xlsx','Mild to severe')
 indp = ["F1T1", "F2T1", "F3T1", "F4T1", "F5T1",
         "S1T1", "S2T1", "S3T1", "S4T1", "S5T1", "S6T1", "S7T1"]
 indn = ["M1T1", "M2T1", "M3T1", "M4T1", "M5T1", "M6T1", "M7T1", "M8T1", "M9T1", "M10T1"]
