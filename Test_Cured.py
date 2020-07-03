6dba1ce48f06f87a3db284522e909ff5c9f9a8b4
Ning-310 <64373769+Ning-310@users.noreply.github.com>
2020-07-03 13:57:43 +0800

    Update Test_Cured.py

--- Test_Cured.py
+++ Test_Cured.py
@@ -4,7 +4,7 @@ import pandas as pd
 import joblib
 
 model=joblib.load("Model/Cured.model")
-df = pd.read_excel('Data/OBCs.xlsx','Cured')
+df = pd.read_excel('Data/Final biomarker combinations.xlsx','Cured')
 indn=["F1T1","F2T1","F3T1","F4T1","F5T1",
      "S1T1","S2T1","S3T1","S4T1","S5T1","S6T1","S7T1",
      "M1T1","M2T1","M3T1","M4T1","M5T1","M6T1","M7T1","M8T1","M9T1","M10T1"]
